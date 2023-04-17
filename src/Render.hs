fa16e3c57ff91d855c02c2f9ef631f8ac3226b6e
blackcap <mailhph@gmail.com>
2023-04-17 11:25:53 +0200

    update recording

--- src/Render.hs
+++ src/Render.hs
@@ -13,34 +13,37 @@ mult s a
   | s > 0 = 1/s - (a/s)
   | let   = a/(-s) + 0.0001
 
-ray po sx sy w h fx@(floor->x) fy@(floor->y) mi fmx@(floor->mx) fmy@(floor->my) m
+ray po purp sx sy w h fx@(floor->x) fy@(floor->y) mi fmx@(floor->mx) fmy@(floor->my) m
   | x >= 0 && y >= 0 && x < w && y < h
   , Just t <- lookupMaps mi mx my m = do
 
     let i = y*w + x
 
-    modify \v -> (i, toFILL t) : v
+    let col | purp = (Unset, Unset, Unset, Set (RGB 64 0 64))
+            | let  = mempty
+
+    modify \v -> (i, toFILL t <> col) : v
 
     let j  = 1
     let jx = sx*j
     let jy = sy*j
 
     case t of
-      Portal _ q@(Pos mi' (V2 x y)) -> do
+      Portal c q@(Pos mi' (V2 x y)) -> do
 
         if not po then do
 
         let ox = fi x + fmx - fi (floor fmx)
         let oy = fi y + fmy - fi (floor fmy)
 
-        ray True sx sy w h (fx+jx) (fy+jy) mi' (ox+jx) (oy+jy) m
+        ray True (c=='+') sx sy w h (fx+jx) (fy+jy) mi' (ox+jx) (oy+jy) m
 
         else do
-        ray True sx sy w h (fx+jx) (fy+jy) mi (fmx+jx) (fmy+jy) m
+        ray True (c=='+') sx sy w h (fx+jx) (fy+jy) mi (fmx+jx) (fmy+jy) m
 
       _ | isOpaque t -> pure ()
         | let        ->
-        ray False sx sy w h (fx+jx) (fy+jy) mi (fmx+jx) (fmy+jy) m
+        ray False purp sx sy w h (fx+jx) (fy+jy) mi (fmx+jx) (fmy+jy) m
 
   | let
   = pure ()
@@ -51,7 +54,7 @@ render w h (Pos mi (V2 mx my)) m = do
   forM_ [0, rot .. 2*pi] \a -> do
     let sx = cos a
     let sy = sin a
-    ray True sx sy w h (fi x + 0.5) (fi y + 0.5) mi (fi mx + 0.5) (fi my + 0.5) m
+    ray True False sx sy w h (fi x + 0.5) (fi y + 0.5) mi (fi mx + 0.5) (fi my + 0.5) m
 
   modify \v -> (y*w+x, (Set '@', None, None, None)) : v
 
