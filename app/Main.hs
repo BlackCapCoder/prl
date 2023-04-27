9379ba56f0629c14f6fc74ae1710bf98f8541070
blackcap <mailhph@gmail.com>
2023-04-27 12:00:01 +0200

    forgot to add files

--- app/Main.hs
+++ app/Main.hs
@@ -2,22 +2,7 @@ module Main where
 
 import Play
 import ANSI
-import System.IO
 
-main = do
-  oldEcho   <- hGetEcho stdout
-  oldBufIn  <- hGetBuffering stdin
-  oldBufOut <- hGetBuffering stdout
-
-  hSetEcho      stdout False
-  hSetBuffering stdin  NoBuffering
-  hSetBuffering stdout (BlockBuffering Nothing)
-
-  putStr $ smcup <> hideCursor
+main = tuiBracket do
   play =<< initialWorld
-  putStr $ rmcup <> showCursor
-
-  hSetEcho      stdout oldEcho
-  hSetBuffering stdin  oldBufIn
-  hSetBuffering stdout oldBufOut
 
